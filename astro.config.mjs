68cbbc3d05c83146281ebf92d8be11a1915881ac
Tu Nombre <tu_email@example.com>
2025-01-20 21:28:20 -0300

    cambiospara deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
 
 export default defineConfig({
   integrations: [tailwind()],
+  vite: {
+    optimizeDeps: {
+      exclude: ['react-compiler-runtime']
+    }
+  }
 });
