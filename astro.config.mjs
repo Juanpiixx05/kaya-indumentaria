c163f0414cbe830db0bcf969d0fb216a13bbe920
Juanpiixx05 <jpbarrientos311005@gmail.com>
2025-02-02 19:45:28 -0300

    testeo mp

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ export default defineConfig({
   vite: {
     envPrefix: ['VITE_'], // Solo expone variables con este prefijo
     server: {
+      host: 'localhost', // Fuerza dirección específica
+      port: 4321, // Puerto fijo
+      strictPort: true, // Evita cambios automáticos
       watch: {
         usePolling: true // Necesario en Windows/WSL
       }
