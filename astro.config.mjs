a4cd50ca6796cb1c2586f7fe19dc7f52c72dcb9b
Tu Nombre <tu_email@example.com>
2025-02-11 18:43:03 -0300

    cambios-subida-vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,27 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
-  output: 'server', // Obligatorio para APIs
+  output: 'static', // Asegúrate de que sea 'static'
+  adapter: vercel(),
   integrations: [react(), tailwind()],
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  },
   vite: {
     define: {
       'process.env.SUPABASE_KEY': JSON.stringify(process.env.SUPABASE_KEY)
     },
     server: {
-      host: 'localhost', // Fuerza dirección específica
-      port: 3000, // Puerto fijo
-      strictPort: true, // Evita cambios automáticos
+      host: 'localhost',
+      port: 3000,
+      strictPort: true,
       watch: {
-        usePolling: true // Necesario en Windows/WSL
+        usePolling: true
       }
     }
   }
