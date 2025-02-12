9e2b8914d40b91f1c285c531a6fbc0c679de2569
Tu Nombre <tu_email@example.com>
2025-02-12 00:56:10 -0300

    cambio vercel.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'static',
-  adapter: vercel(),
+  output: 'server',
+  adapter: vercelServerless(),
   integrations: [
     react({
       include: ['**/*.jsx', '**/*.tsx'],
